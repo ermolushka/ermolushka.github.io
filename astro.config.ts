7093ba672838c29804886cf4b2b3a838a6f0a535
Alexey Ermolaev <alexeyermolaev91@gmail.com>
2025-01-18 19:11:56 +0200

    added google analytics

--- astro.config.ts
+++ astro.config.ts
@@ -5,6 +5,8 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
+import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,6 +17,12 @@ export default defineConfig({
     }),
     react(),
     sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    mdx(),
   ],
   markdown: {
     remarkPlugins: [
