b37f11c421ed2b2581f7add6239603b70c9e5553
ermolushka <alexeyermolaev91@gmail.com>
2026-06-24 00:04:05 +0300

    mobile friendly

--- .astro/assets.mjs
+++ .astro/assets.mjs
@@ -1,4 +1,1 @@
-
-import __ASTRO_IMAGE_IMPORT_sJHC9 from '@assets/images/snake-wasm.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fwasm-snake-game-rust.md';
-export default new Map([["@assets/images/snake-wasm.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fwasm-snake-game-rust.md", __ASTRO_IMAGE_IMPORT_sJHC9]]);
-		
+export default new Map();
